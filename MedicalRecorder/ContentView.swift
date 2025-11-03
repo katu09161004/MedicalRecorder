08eef99a434086758dd499c37c229c8309f1a8d9
KATSUYOSHI FUJITA <katu09161004@gmail.com>
2025-11-03 20:27:16 +0900

    Initial Commit

---
+++ MedicalRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedicalRecorder
+//
+//  Created by Katsuyoshi Fujita on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedicalRecorder/MedicalRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicalRecorderApp.swift
+//  MedicalRecorder
+//
+//  Created by Katsuyoshi Fujita on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MedicalRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
